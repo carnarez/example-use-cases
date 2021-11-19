211581cc2ad918e6b1f4ad5175d5a48af8cb6db5
carnarez <11967125+carnarez@users.noreply.github.com>
2021-11-19 22:50:19 +0100

    Extra warning in producer folder README.md

--- incoming-traffic/consumer/pull.py
+++ incoming-traffic/consumer/pull.py
@@ -1,13 +1,13 @@
-"""Check for and read new base64-encoded content from the eventhub."""
+"""Check for and read new base64-encoded content from the event hub."""
 
-# import base64
 import os
 import sys
 import time
 
 from redis import Redis
 
-# adding an auto-decoding
+# connect to redis
+# adding auto-decoding to ease processing (python dictionary)
 engine: Redis = Redis(
     password=os.environ["REDIS_PASSWORD"],
     host=os.environ["REDIS_HOST"],
@@ -21,6 +21,7 @@ while True:
     events = engine.xread({"snapshots": "0-0"}, None, 0)
     for (id_, payload) in events[0][1]:
 
+        # extend the code here
         print(
             f'{payload["sensor"]} @ {payload["stamp"]} :',
             f'{payload["capture"][:18]}...{payload["capture"][-18:]}'.encode()
--- incoming-traffic/producer/README.md
+++ incoming-traffic/producer/README.md
@@ -1,2 +1,4 @@
 Producer posting [`Base64`](https://en.wikipedia.org/wiki/Base64)-encoded images to the
 `snapshots` feed managed by [`Redis Streams`](https://redis.io/topics/streams-intro).
+
+The code in this folder should not need to be modified. Check the `consumer/` instead.
